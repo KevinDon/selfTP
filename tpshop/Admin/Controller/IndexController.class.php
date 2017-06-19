3c44032915fb69beb3d5f3878114825107a5b218
Kevin Don <kevintang002@gmail.com>
2017-06-19 22:35:23 +0800

    更新RBAC
    添加RBAC的用户权限控制

--- tpshop/Admin/Controller/IndexController.class.php
+++ tpshop/Admin/Controller/IndexController.class.php
@@ -18,7 +18,7 @@
 		}
 
 		public function right(){
-			$this->display('right');
+			$this->display('right');      
 		}
 
 		public function head(){
@@ -26,6 +26,16 @@
 		}
 		
 		public function left(){
+                        $name = session('managerName');
+                        $model = M();
+                        //获取改用户的role ids
+                        $sql = "select b.role_auth_ids from sw_manager a join sw_role b on a.mg_role_id = b.role_id where a.mg_name='" . $name . "' ";
+                        $info = $model->query($sql);
+                        $auth_ids = $info[0]['role_auth_ids'];
+                        //根据IDs获取具体权限
+                        $sql_ID = "select * from sw_auth where auth_id in ($auth_ids) ";
+                        $auth_info = $model->query($sql_ID);
+                        $this->assign('auth_info', $auth_info);
 			$this->display('left');
 		}
 
