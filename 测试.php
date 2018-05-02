5d5d7376e5d12564764f1c11e60f2da53404f550
Kevin Don <kevintang002@gmail.com>
2018-05-02 18:38:14 +0800

    测试私人电脑的PHP Storm链接GitHub

--- 测试.php
+++ 测试.php
@@ -1,10 +1,5 @@
 <?php
-    /*create by intellij IDEA*/
-	echo "这是测试代码文件";
-    echo "这是IDEA测试代码";
-    echo "这是IDEA github代码";
+
+    /* create by PHP Storm */
     echo "这是php Storm";
-
-
-
     var_dump("online change");
